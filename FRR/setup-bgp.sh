5215de3ed2769637b4b58e87304edc1550d6aafb
Abdul Basit <59034807+abasitt@users.noreply.github.com>
2023-04-14 14:10:40 +0800

    Create setup-bgp.sh

---
+++ FRR/setup-bgp.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e
+
+# Specify the IPv6 addresses in an array
+ipv6_addrs=(
+    "fd74:ca9b:3a09:868c:10:9:65:1"
+    "fd74:ca9b:3a09:868c:10:9:65:2"
+)
+
+local_as=65100
+
+remote_as=64500
+
+docker exec frr01 vtysh \
+    -c "configure terminal" \
+    -c "router bgp $local_as" \
+    -c "no bgp ebgp-requires-policy" \
+    -c "no bgp default ipv4-unicast" \
+    -c "no bgp network import-check"
+
+for addr in "${ipv6_addrs[@]}"
+do
+    # Configure BGP peering using the current IPv6 address and remote AS
+    docker exec frr01 vtysh \
+    -c "configure terminal" \
+    -c "router bgp $local_as" \
+    -c "neighbor $addr remote-as $remote_as" \
+    -c "address-family ipv6 unicast" \
+    -c "neighbor $addr activate" \
+    -c "exit" \
+    -c "exit"
+done
