e41a1377438455aa69e23750855905904565a669
abasitt <a.basit3028@gmail.com>
2023-07-26 07:45:12 -0700

    demo app

--- istio/multicluster/apps/installsampleapps.sh
+++ istio/multicluster/apps/installsampleapps.sh
@@ -12,24 +12,28 @@ TAG=1.18.1
 echo "Pulling images to a docker host..."
 docker pull docker.io/istio/examples-helloworld-v1
 docker pull docker.io/istio/examples-helloworld-v2
+docker pull docker.io/kong/httpbin
 docker pull curlimages/curl
 
 echo "Load images to kind clusters..."
 kind load docker-image --name $CLUSTER1_NAME docker.io/istio/examples-helloworld-v1
 kind load docker-image --name $CLUSTER1_NAME docker.io/istio/examples-helloworld-v2
 kind load docker-image --name $CLUSTER1_NAME curlimages/curl
+kind load docker-image --name $CLUSTER1_NAME docker.io/kong/httpbin
 
 kind load docker-image --name $CLUSTER2_NAME docker.io/istio/examples-helloworld-v1
 kind load docker-image --name $CLUSTER2_NAME docker.io/istio/examples-helloworld-v2
 kind load docker-image --name $CLUSTER2_NAME curlimages/curl
+kind load docker-image --name $CLUSTER2_NAME docker.io/kong/httpbin
 
 
 echo "Install demo apps to kind clusters..."
 kubectl --context="${CLUSTER1_CTX}" label ns default istio-injection=enabled --overwrite
-kubectl --context="${CLUSTER1_CTX}" apply -f https://raw.githubusercontent.com/istio/istio/master/samples/sleep/sleep.yaml
+#kubectl --context="${CLUSTER1_CTX}" apply -f https://raw.githubusercontent.com/istio/istio/master/samples/sleep/sleep.yaml
 #kubectl --context="${CLUSTER1_CTX}" apply -f https://raw.githubusercontent.com/istio/istio/master/samples/helloworld/helloworld.yaml
 
 
 kubectl --context="${CLUSTER2_CTX}" label ns default istio-injection=enabled --overwrite
 kubectl --context="${CLUSTER2_CTX}" apply -f https://raw.githubusercontent.com/istio/istio/master/samples/sleep/sleep.yaml
 kubectl --context="${CLUSTER2_CTX}" apply -f https://raw.githubusercontent.com/istio/istio/master/samples/helloworld/helloworld.yaml
+kubectl --context="${CLUSTER2_CTX}" apply -f https://raw.githubusercontent.com/istio/istio/master/samples/httpbin/httpbin.yaml
