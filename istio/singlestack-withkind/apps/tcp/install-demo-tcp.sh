420b73c397f0b37115fae1005c74d74cce7c1f68
abasitt <a.basit3028@gmail.com>
2023-07-31 18:29:27 -0700

    updating apps

--- istio/singlestack-withkind/apps/tcp/install-demo-tcp.sh
+++ istio/singlestack-withkind/apps/tcp/install-demo-tcp.sh
@@ -6,11 +6,13 @@ CLUSTER_CTX=kind-ipv4
 # load tcp echo images to kind
 echo "Pulling images to a docker host..."
 docker pull docker.io/istio/tcp-echo-server:1.2
+docker pull curlimages/curl
 
 echo "Load images to kind clusters..."
 kind load docker-image --name $CLUSTER_NAME docker.io/istio/tcp-echo-server:1.2
-#kind load docker-image --name $CLUSTER1_NAME docker.io/istio/examples-helloworld-v2
+kind load docker-image --name $CLUSTER_NAME curlimages/curl
 
 echo "Install demo apps to kind clusters..."
 kubectl --context="${CLUSTER_CTX}" label ns default istio-injection=enabled --overwrite
-kubectl --context="${CLUSTER_CTX}" apply -f https://github.com/istio/istio/blob/master/samples/tcp-echo/tcp-echo-ipv4.yaml
+kubectl --context="${CLUSTER_CTX}" apply -f https://raw.githubusercontent.com/istio/istio/master/samples/tcp-echo/tcp-echo.yaml
+kubectl --context="${CLUSTER_CTX}" apply -f https://raw.githubusercontent.com/istio/istio/master/samples/sleep/sleep.yaml
