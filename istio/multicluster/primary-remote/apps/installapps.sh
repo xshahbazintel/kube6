e340c4ae00681c67e9671f08258911601a7953b1
abasitt <abdul.basit@rakuten.com>
2023-10-31 01:23:11 +0000

    updating istio multcluster script

--- istio/multicluster/primary-remote/apps/installapps.sh
+++ istio/multicluster/primary-remote/apps/installapps.sh
@@ -11,25 +11,25 @@ docker pull docker.io/kong/httpbin
 docker pull curlimages/curl
 
 echo "Load images to kind clusters..."
-kind load docker-image --name $CLUSTER1_NAME docker.io/istio/examples-helloworld-v1
-kind load docker-image --name $CLUSTER1_NAME docker.io/istio/examples-helloworld-v2
-kind load docker-image --name $CLUSTER1_NAME curlimages/curl
-#kind load docker-image --name $CLUSTER1_NAME docker.io/kong/httpbin
+kind load docker-image --name $CLUSTER7_NAME docker.io/istio/examples-helloworld-v1
+kind load docker-image --name $CLUSTER7_NAME docker.io/istio/examples-helloworld-v2
+kind load docker-image --name $CLUSTER7_NAME curlimages/curl
+#kind load docker-image --name $CLUSTER7_NAME docker.io/kong/httpbin
 
-kind load docker-image --name $CLUSTER2_NAME docker.io/istio/examples-helloworld-v1
-kind load docker-image --name $CLUSTER2_NAME docker.io/istio/examples-helloworld-v2
-kind load docker-image --name $CLUSTER2_NAME curlimages/curl
-#kind load docker-image --name $CLUSTER2_NAME docker.io/kong/httpbin
+kind load docker-image --name $CLUSTER8_NAME docker.io/istio/examples-helloworld-v1
+kind load docker-image --name $CLUSTER8_NAME docker.io/istio/examples-helloworld-v2
+kind load docker-image --name $CLUSTER8_NAME curlimages/curl
+#kind load docker-image --name $CLUSTER8_NAME docker.io/kong/httpbin
 
 echo "Install demo apps to kind clusters..."
-kubectl --context="${CLUSTER1_CTX}" label ns default istio-injection=enabled --overwrite
-kubectl --context="${CLUSTER1_CTX}" apply -f https://raw.githubusercontent.com/istio/istio/master/samples/sleep/sleep.yaml
-kubectl --context="${CLUSTER1_CTX}" apply -f https://raw.githubusercontent.com/istio/istio/master/samples/helloworld/helloworld.yaml -l service=helloworld
-kubectl --context="${CLUSTER1_CTX}" apply -f https://raw.githubusercontent.com/istio/istio/master/samples/helloworld/helloworld.yaml -l version=v1
+kubectl --context="${CLUSTER7_CTX}" label ns default istio-injection=enabled --overwrite
+kubectl --context="${CLUSTER7_CTX}" apply -f https://raw.githubusercontent.com/istio/istio/master/samples/sleep/sleep.yaml
+kubectl --context="${CLUSTER7_CTX}" apply -f https://raw.githubusercontent.com/istio/istio/master/samples/helloworld/helloworld.yaml -l service=helloworld
+kubectl --context="${CLUSTER7_CTX}" apply -f https://raw.githubusercontent.com/istio/istio/master/samples/helloworld/helloworld.yaml -l version=v1
 
 
-kubectl --context="${CLUSTER2_CTX}" label ns default istio-injection=enabled --overwrite
-kubectl --context="${CLUSTER2_CTX}" apply -f https://raw.githubusercontent.com/istio/istio/master/samples/sleep/sleep.yaml
-kubectl --context="${CLUSTER2_CTX}" apply -f https://raw.githubusercontent.com/istio/istio/master/samples/helloworld/helloworld.yaml -l service=helloworld
-kubectl --context="${CLUSTER2_CTX}" apply -f https://raw.githubusercontent.com/istio/istio/master/samples/helloworld/helloworld.yaml -l version=v2
-#kubectl --context="${CLUSTER2_CTX}" apply -f https://raw.githubusercontent.com/istio/istio/master/samples/httpbin/httpbin.yaml 
+kubectl --context="${CLUSTER8_CTX}" label ns default istio-injection=enabled --overwrite
+kubectl --context="${CLUSTER8_CTX}" apply -f https://raw.githubusercontent.com/istio/istio/master/samples/sleep/sleep.yaml
+kubectl --context="${CLUSTER8_CTX}" apply -f https://raw.githubusercontent.com/istio/istio/master/samples/helloworld/helloworld.yaml -l service=helloworld
+kubectl --context="${CLUSTER8_CTX}" apply -f https://raw.githubusercontent.com/istio/istio/master/samples/helloworld/helloworld.yaml -l version=v2
+#kubectl --context="${CLUSTER8_CTX}" apply -f https://raw.githubusercontent.com/istio/istio/master/samples/httpbin/httpbin.yaml 
--- istio/multicluster/primary-remote/apps/troubleshoot.sh
+++ istio/multicluster/primary-remote/apps/troubleshoot.sh
@@ -2,43 +2,43 @@
 
 . ../../config.sh
 
-echo "Block $CLUSTER2_NAME EastWest GW Port 15443 using Cilium NetPol..."
-kubectl --context="${CLUSTER2_CTX}" apply -f ../cilium/deny15443.yaml
+echo "Block $CLUSTER8_NAME EastWest GW Port 15443 using Cilium NetPol..."
+kubectl --context="${CLUSTER8_CTX}" apply -f ../cilium/deny15443.yaml
 # apply timeout for helloworld
-kubectl --context="${CLUSTER1_CTX}" apply -f vs.yaml
-kubectl --context="${CLUSTER2_CTX}" apply -f vs.yaml
+kubectl --context="${CLUSTER7_CTX}" apply -f vs.yaml
+kubectl --context="${CLUSTER8_CTX}" apply -f vs.yaml
 
 
-echo "$CLUSTER1_NAME Eastwest Gateways LB IP address..."
+echo "$CLUSTER7_NAME Eastwest Gateways LB IP address..."
 echo $(kubectl \
-    --context="${CLUSTER1_CTX}" \
+    --context="${CLUSTER7_CTX}" \
     -n istio-system get svc istio-eastwestgateway \
     -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
 
-echo "$CLUSTER2_NAME Eastwest Gateways LB IP address..."
+echo "$CLUSTER8_NAME Eastwest Gateways LB IP address..."
 echo $(kubectl \
-    --context="${CLUSTER2_CTX}" \
+    --context="${CLUSTER8_CTX}" \
     -n istio-system get svc istio-eastwestgateway \
     -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
 
 echo "*************************"
 
-echo "$CLUSTER1_NAME Sleep endpoints of helloworld..."
-istioctl pc ep deploy/sleep -n default --cluster "outbound|5000||helloworld.default.svc.cluster.local" --context="${CLUSTER1_CTX}"
-echo "$CLUSTER2_NAME Sleep endpoints of helloworld..."
-istioctl pc ep deploy/sleep -n default --cluster "outbound|5000||helloworld.default.svc.cluster.local" --context="${CLUSTER2_CTX}"
+echo "$CLUSTER7_NAME Sleep endpoints of helloworld..."
+istioctl pc ep deploy/sleep -n default --cluster "outbound|5000||helloworld.default.svc.cluster.local" --context="${CLUSTER7_CTX}"
+echo "$CLUSTER8_NAME Sleep endpoints of helloworld..."
+istioctl pc ep deploy/sleep -n default --cluster "outbound|5000||helloworld.default.svc.cluster.local" --context="${CLUSTER8_CTX}"
 
 echo "*************************"
 
 #load helloworld and sleep images
-echo "Curl from cluster $CLUSTER1_NAME sleep..."
-for i in {1..6}; do kubectl --context="${CLUSTER1_CTX}" exec deploy/sleep -- curl -sS helloworld:5000/hello; sleep 1; done
+echo "Curl from cluster $CLUSTER7_NAME sleep..."
+for i in {1..6}; do kubectl --context="${CLUSTER7_CTX}" exec deploy/sleep -- curl -sS helloworld:5000/hello; sleep 1; done
 #load helloworld and sleep images
-echo "Curl from cluster $CLUSTER2_NAME sleep..."
-for i in {1..6}; do kubectl --context="${CLUSTER2_CTX}" exec deploy/sleep -- curl -sS helloworld:5000/hello; sleep 1; done
+echo "Curl from cluster $CLUSTER8_NAME sleep..."
+for i in {1..6}; do kubectl --context="${CLUSTER8_CTX}" exec deploy/sleep -- curl -sS helloworld:5000/hello; sleep 1; done
 
-echo "Remove $CLUSTER2_NAME EastWest GW Port 15443 Cilium NetPol..."
-kubectl --context="${CLUSTER2_CTX}" delete -f ../cilium/deny15443.yaml
+echo "Remove $CLUSTER8_NAME EastWest GW Port 15443 Cilium NetPol..."
+kubectl --context="${CLUSTER8_CTX}" delete -f ../cilium/deny15443.yaml
 # delete timeout for helloworld
-kubectl --context="${CLUSTER1_CTX}" delete -f vs.yaml
-kubectl --context="${CLUSTER2_CTX}" delete -f vs.yaml
+kubectl --context="${CLUSTER7_CTX}" delete -f vs.yaml
+kubectl --context="${CLUSTER8_CTX}" delete -f vs.yaml
