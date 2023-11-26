a15f6c59d7fddab528fdba6b332123f476e4bfd8
abasitt <abdul.basit@rakuten.com>
2023-11-26 04:47:42 +0000

    updating installapp kubeday

--- cilium/kubedaySG2023/apps/installapps.sh
+++ cilium/kubedaySG2023/apps/installapps.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-. ../../config.sh
+. ../config.sh
 
 #load helloworld and sleep images
 
 echo "Pulling images to a docker host..."
 docker pull docker.io/istio/examples-helloworld-v1
 docker pull docker.io/istio/examples-helloworld-v2
-docker pull docker.io/kong/httpbin
+#docker pull docker.io/kong/httpbin
 docker pull curlimages/curl
 
 echo "Load images to kind clusters..."
@@ -22,14 +22,16 @@ kind load docker-image --name $CLUSTER2_NAME curlimages/curl
 #kind load docker-image --name $CLUSTER2_NAME docker.io/kong/httpbin
 
 echo "Install demo apps to kind clusters..."
-kubectl --context="${CLUSTER1_CTX}" label ns default istio-injection=enabled --overwrite
 kubectl --context="${CLUSTER1_CTX}" apply -f https://raw.githubusercontent.com/istio/istio/master/samples/sleep/sleep.yaml
 kubectl --context="${CLUSTER1_CTX}" apply -f https://raw.githubusercontent.com/istio/istio/master/samples/helloworld/helloworld.yaml -l service=helloworld
 kubectl --context="${CLUSTER1_CTX}" apply -f https://raw.githubusercontent.com/istio/istio/master/samples/helloworld/helloworld.yaml -l version=v1
 
 
-kubectl --context="${CLUSTER2_CTX}" label ns default istio-injection=enabled --overwrite
 kubectl --context="${CLUSTER2_CTX}" apply -f https://raw.githubusercontent.com/istio/istio/master/samples/sleep/sleep.yaml
 kubectl --context="${CLUSTER2_CTX}" apply -f https://raw.githubusercontent.com/istio/istio/master/samples/helloworld/helloworld.yaml -l service=helloworld
 kubectl --context="${CLUSTER2_CTX}" apply -f https://raw.githubusercontent.com/istio/istio/master/samples/helloworld/helloworld.yaml -l version=v2
 #kubectl --context="${CLUSTER2_CTX}" apply -f https://raw.githubusercontent.com/istio/istio/master/samples/httpbin/httpbin.yaml 
+
+# patch services to enable cilium global service discovery
+kubectl --context="${CLUSTER1_CTX}" patch service helloworld --type='json' -p='[{"op": "add", "path": "/metadata/annotations/service.cilium.io~1global", "value": "true"}]'
+kubectl --context="${CLUSTER2_CTX}" patch service helloworld --type='json' -p='[{"op": "add", "path": "/metadata/annotations/service.cilium.io~1global", "value": "true"}]'
