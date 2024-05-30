2cd63f738a79ec2b6d6cb4b47bcbdec73e7c938f
abasitt <abdul.basit@rakuten.com>
2024-05-30 09:36:39 +0000

    trying debug image

--- calico/ipv6_multus/kind/install-multus-thin.sh
+++ calico/ipv6_multus/kind/install-multus-thin.sh
@@ -3,11 +3,13 @@
 # download docker images to a host
 echo "Pulling multus images to a docker host..."
 docker pull ghcr.io/k8snetworkplumbingwg/multus-cni:v4.0.2
+docker pull ghcr.io/k8snetworkplumbingwg/multus-cni:v4.0.2-debug
 #docker pull ghcr.io/k8snetworkplumbingwg/multus-cni:v4.0.2-thick
 
 # Load images to both clusters
 echo "Loading alpine image to both clusters..."
 kind load docker-image --name $CLUSTER1_NAME ghcr.io/k8snetworkplumbingwg/multus-cni:v4.0.2
+kind load docker-image --name $CLUSTER1_NAME ghcr.io/k8snetworkplumbingwg/multus-cni:v4.0.2-debug
 #kind load docker-image --name $CLUSTER1_NAME ghcr.io/k8snetworkplumbingwg/multus-cni:v4.0.2-thick
 
 # Install multus thin plugin
